47f51d7777b0ba02c3219ef569d97121f4b8a80f
Cru <cruccu.lorenzo@studenti-ittfedifermi.edu.it>
2023-11-04 14:01:38 +0100

    capacitor init

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,17 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Lionard LTS',
-  webDir: 'www',
-  server: {
-    androidScheme: 'https'
-  }
+	plugins: {
+		CapacitorHttp: {
+			enabled: true,
+		},
+	},
+	appId: 'io.ionic.starter',
+	appName: 'SOS Toscana',
+	webDir: 'www',
+	server: {
+		androidScheme: 'https'
+	}
 };
 
 export default config;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+	selector: 'app-root',
+	templateUrl: 'app.component.html',
+	styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  public appPages = [
-    { title: 'Acquirenti', url: '/folder/acquirenti', icon: 'mail' },
-    { title: 'Agenda', url: '/folder/agenda', icon: 'paper-plane' },
-    { title: 'Immobili', url: '/folder/immobili', icon: 'map' },
-    { title: 'Rubrica', url: '/folder/rubrica', icon: 'archive' },
-    { title: 'Mappa', url: '/folder/maps', icon: 'map' },
-  ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor() {}
+	public appPages = [
+		{ title: 'Richiesta di aiuto', url: '/folder/sos', icon: 'archive' },
+	];
+	public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+	constructor() { }
 }
